ea2152593d2e45a0c07fb447b9f90bfa29a179c8
nest <sankarisiva2462@gmail.com>
2022-08-16 13:00:38 +0530

    git done

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+
+    constructor(private userservice: UserService) { }
+
+    @Get()
+    data(){
+        console.log("data getting successfully..")
+    }
+
+}
